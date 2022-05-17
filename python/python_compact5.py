10a583d3af3edf5ab614a370e89c0f8314925698
Hermann-SW <hermann@stamm-wilbrandt.de>
2022-05-17 22:37:39 +0200

    Add "_" #define timestamping from C++ to python_compact5.py demo

--- python/python_compact5.py
+++ python/python_compact5.py
@@ -1,19 +1,31 @@
 ''' python_compact5.py '''
-from sys import argv
-
 #include "undirected_graph.py"
 #include "fullerenes.py"
 
-sel = argv[1] if len(argv) > 1 else  "../graphs/10.a"
+#include "python_compact5.yp"
 
-L = parse2file(sel)
-G = from_adjacency_list(L)
+def _main(argc, argv):
+    sel = argv[1] if argc > 1 else  "../graphs/10.a"
+    _
+    L = parse2file(sel)
+    _
+    G = from_adjacency_list(L)
+    _
+    for (v, l) in enumerate(L):
+        for (j, w) in enumerate(l):
+            assert w == opposite(G, v, G.V[v][j])
 
-for (v, l) in enumerate(L):
-    for (j, w) in enumerate(l):
-        assert w == opposite(G, v, G.V[v][j])
+    _
+    assert is_embedding(G)
+    _
+    compact5_traversal(G, compact5_traversal_visitor())
+    _
+    assert max_degree(G) <= 5
+    _
 
-print("identical vertex ordering in adjacency list and graph verified")
+    print("identical vertex ordering in adjacency list and graph verified")
+    print("is_embedding(" + str(sel) + ") verified, has " + str(n_faces_planar(G)) + " faces")
+    print("compat5_traversal(G, {}) done")
+    print("maxdegree(G) <=5 verified")
 
-assert is_embedding(G)
-print("is_embedding(" + str(sel) + ") verified, has " + str(n_faces_planar(G)) + " faces")
+_summary()
