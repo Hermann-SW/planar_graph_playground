e35c143e39e09bf2e284ef0500407e170948c48d
Hermann-SW <hermann@stamm-wilbrandt.de>
2022-06-23 22:53:34 +0200

    Next step in iterative 3D sphere spring embedder

--- node.straight_line_drawing.3D.js
+++ node.straight_line_drawing.3D.js
@@ -18,7 +18,10 @@ var face = [];
 var e = any_edge(G);
 var v = source(G, e);
 var coords2D;
-var V = (process.argv.length > 4) ? parseInt(process.argv[4]) : 0;
+var s;
+var V;
+var sum;
+var nor;
 
 function rad2deg(r) {
     return r / Math.PI * 180;
@@ -28,14 +31,39 @@ function length_2D(x, y) {
     return Math.hypot(x, y);
 }
 
+function length_3D(v) {
+    return Math.hypot(v[0], v[1], v[2]);
+}
+
+function dist_3D(p, q) {
+    return length_3D(sub_3D(p, q));
+}
+
+function add_3D(p, q) {
+    return [p[0]+q[0],p[1]+q[1],p[2]+q[2]];
+}
+
+function sub_3D(p, q) {
+    return [p[0]-q[0],p[1]-q[1],p[2]-q[2]];
+}
+
 function scale_3D(v, f) {
     return [f * v[0], f * v[1], f * v[2]];
 }
 
+
+function norm_3D(v) {
+    return scale_3D(v, 1 / length_3D(v));
+}
+
 function map_3D(x, y) {
     var a = Math.atan2(y, x);
-    var l = 0.8 * Math.PI * Math.sqrt(length_2D(x, y));
+    var l = 0.9 * Math.PI * length_2D(x, y);
     return [Math.sin(a) * Math.sin(l), Math.cos(a) * Math.sin(l), Math.cos(l)];
+}
+
+function center_3D(p, q, r) {
+    return [(p[0]+q[0]+r[0])/3,(p[1]+q[1]+r[1])/3,(p[2]+q[2]+r[2])/3];
 }
 
 function straight_line_drawing_3D(G) {
@@ -77,8 +105,36 @@ forall_vertices(G, function (v) {
     coords[v] = map_3D(coords2D[0][v], coords2D[1][v]);
 });
 
+sum = filled_array(n_vertices(G), 1, [0,0,0]);
+nor = filled_array(n_vertices(G), 1, [0,0,0]);
+
+forall_vertices(G, function (v) {
+    var visited = filled_array(n_edges(G), 2, false);
+    var face = [];
+    forall_incident_edges(G, v, function(e) {
+        traverse_face(G, visited, v, e, ind(G, v, e), {next_vertex: function (w) {
+            if (v !== w) {
+                face.push(w);
+            }
+        }});
+    });
+    face.forEach(function (x) {
+        sum[v] = add_3D(sum[v], coords[x]);
+    });
+    nor[v] = norm_3D(sum[v]);
+});
+
+V = 0;
+forall_vertices(G, function (v) {
+    if (dist_3D(nor[V], coords[V]) < dist_3D(nor[v], coords[v])) {
+        V = v;
+    }
+});
+
 console.log("$vpr = [",-rad2deg(Math.acos(coords[V][2])),",0,",
                        -rad2deg(Math.atan2(coords[V][0], coords[V][1])),"];");
+
+coords[V] = nor[V];
 
 forall_vertices(G, function (v) {
     coords[v] = scale_3D(coords[v], Math.sqrt(n_vertices(G)));
